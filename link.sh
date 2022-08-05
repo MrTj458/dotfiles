13103b03d0861379a72621b739d404e8fe6d05a7
MrTj458 <mrtj458@gmail.com>
2022-08-05 17:41:20 -0600

    Added tmux configurations

--- link.sh
+++ link.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
 ln -nfs $HOME/.dotfiles/.zshrc $HOME/.zshrc
+ln -nfs $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf 
 ln -nfs $HOME/.dotfiles/init.vim $HOME/.config/nvim/init.vim
 ln -nfs $HOME/.dotfiles/init.lua $HOME/.config/nvim/lua/init.lua
