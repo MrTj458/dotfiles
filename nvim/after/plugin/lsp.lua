5dcf9ed062cbce468e45f5a2b8ce61c1b423237b
MrTj458 <mrtj458@gmail.com>
2024-02-21 20:35:13 -0700

    Fixed LSP keybinds

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,7 +1,15 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
+    lsp_zero.default_keymaps({
+        buffer = bufnr,
+        preserve_mappings = false,
+    })
+
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]et [R]eferences', buffer = bufnr })
+    vim.keymap.set('n', 'gs', require('telescope.builtin').lsp_document_symbols, { desc = '[G]et [S]ymbols', buffer = bufnr })
+    vim.keymap.set('n', 'gi', require('telescope.builtin').lsp_implementations, { desc = '[G]et [I]mplementations', buffer = bufnr })
+    vim.keymap.set('n', '<leader>gd', require('telescope.builtin').diagnostics, { desc = '[G]et [D]iagnostics', buffer = bufnr })
 end)
 
 require('mason').setup({})
@@ -36,6 +44,7 @@ local cmp = require('cmp')
 cmp.setup({
     mapping = {
         ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-j>'] = cmp.mapping.select_next_item(),
         ['<C-k>'] = cmp.mapping.select_prev_item(),
     }
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -21,7 +21,6 @@ vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
@@ -30,6 +29,3 @@ vim.keymap.set('n', '<leader>/', function()
     previewer = false,
 })
 end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', 'gr', builtin.lsp_references, { desc = '[G]et [R]eferences' })
-vim.keymap.set('n', 'gs', builtin.lsp_document_symbols, { desc = '[G]et [S]ymbols' })
