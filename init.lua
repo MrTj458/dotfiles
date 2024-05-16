c9931fc3584c93e3cee0dd770dd0779d3fe59832
MrTj458 <mrtj458@gmail.com>
2024-05-15 22:02:05 -0600

    Indent keep highlighted

--- init.lua
+++ init.lua
@@ -109,6 +109,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- Keep lines highlighted when indenting
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -652,7 +656,9 @@ require("lazy").setup({
 	},
 	{
 		"ray-x/lsp_signature.nvim",
-		opts = {},
+		opts = {
+			hint_enable = false,
+		},
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
